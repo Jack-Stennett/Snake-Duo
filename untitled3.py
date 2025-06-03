0be7cea4f21aa3bbe9467aa418e2533006204fe4
Jack Stennett <jack.stennett@sciencespo.fr>
2025-06-04 00:37:34 +0200

    Add remaining project files

---
+++ untitled3.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 30 22:31:15 2025
+
+@author: jack_
+"""
+
+head = [1,2,3,4,5,6]
+
+print (head)
+
+head[x:]
+
+    x = int(len(head)/2)
+print(x)
+
+
+name = "Edmond"
+months = 6
+print(f"my name is {name} and I am {months} months old")
